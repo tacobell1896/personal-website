b5bc2f10b1ef561fcec3330a0e9184b1b06c1526
Turner Bell <turner@tbell.us>
2024-07-25 01:44:34 -0400

    define repo type

--- app/portfolio/github-portfolio.tsx
+++ app/portfolio/github-portfolio.tsx
@@ -1,3 +1,5 @@
+import { Key } from "react";
+
 export default async function GithubPortfolio() {
   const url = `https://api.github.com/users/tacobell1896/repos`;
 
@@ -9,14 +11,21 @@ export default async function GithubPortfolio() {
   const data = await response.json();
   return (
     <ul className="m-auto ">
-      {data.map((repo) => (
-        <li key={repo.id} className="rounded-md border m-5 p-5">
-          Repo: <a href={repo.html_url}>{repo.name}</a>
-          {repo.description ? (
-            <p>Description: {repo.description}</p>
-          ) : null}{" "}
-        </li>
-      ))}
+      {data.map(
+        (repo: {
+          id: Key | null | undefined;
+          html_url: string | undefined;
+          name: string | null | undefined;
+          description: string | null | undefined;
+        }) => (
+          <li key={repo.id} className="rounded-md border m-5 p-5">
+            Repo: <a href={repo.html_url}>{repo.name}</a>
+            {repo.description ? (
+              <p>Description: {repo.description}</p>
+            ) : null}{" "}
+          </li>
+        ),
+      )}
     </ul>
   );
 }

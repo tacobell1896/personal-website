6e14d37300a58ac2df23325df4ca3c16699496c6
Turner Bell <turner@tbell.us>
2024-08-07 19:41:02 -0400

    add link data

--- app/homepage/link-section.tsx
+++ app/homepage/link-section.tsx
@@ -1,7 +1,15 @@
+import { Key } from "react";
+import links from ".links.json";
+
 export default function LinkSection() {
   return (
     <section className="flex flex-col items-center justify-center">
-        <h1 className="text-lg">Links</h1>
+      <h1 className="text-lg">Links</h1>
+      <ul className="flex flex-col items-center justify-center">
+        <li className="text-sm">
+          <a href="{link.url}">{link.name}</a>
+        </li>
+      </ul>
     </section>
   );
 }

04f481dbdc730e1e55d8bdc422ec09fac253c17a
Turner Bell <tbell@triad-inc.com>
2024-11-11 12:58:16 -0500

    center text and fix input method

--- app/homepage/page.tsx
+++ app/homepage/page.tsx
@@ -3,13 +3,12 @@ import Clock from "./clock";
 export default function Homepage() {
   return (
     <main>
-      <h1 className="text-xl">Welcome ~</h1>
-      <h2 className="text-lg">
+      <h1 className="text-xl text-center">Welcome ~</h1>
+      <h2 className="text-lg text-center">
         The time is <Clock />
       </h2>
       <form action="https://google.com/search" method="get">
-        <input type="text" name="q" placeholder="Search for something..." />
-        <button type="submit">Search</button>
+        <input className="w-full text-center rounded border-white bg-transparent " type="text" name="q" placeholder="Search for something..." />
       </form>
       <LinkSection />
     </main>
